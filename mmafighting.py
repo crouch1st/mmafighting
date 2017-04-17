412d73ea8c10e2b2ab069ebbdb8c1fe92b1f3acd
Michael Rodriguez <michael.rodriguez@drillinginfo.com>
2017-04-17 07:59:28 -0500

    add email enhancement

--- mmafighting.py
+++ mmafighting.py
@@ -4,6 +4,7 @@ import fnmatch
 import urllib.request
 import logging
 from bs4 import BeautifulSoup
+import smtplib
 
 
 logger = logging.getLogger(__name__)
@@ -19,15 +20,18 @@ def decode_text(string, character_decoding):
     return string.decode(character_decoding)
 
 
-def get_tag_link(url_prefix, links, zip_wildcard):
+def get_tag_link(links, wildcard):
     """
     :param url_prefix: https prefix to be concatenated with tag link
     :param links: tuple of tag links from parsed html
     :param zip_wildcard: string of file type to be searched
     :return: url for downloading desired file
     """
-    matching = fnmatch.filter(links, zip_wildcard)
-    return url_prefix + matching[0]
+    try:
+        matching = fnmatch.filter(links, wildcard)
+        return matching
+    except:
+        return []
 
 
 def get_tag_list(url, parser, tag1, tag2, character_decoding):
@@ -51,6 +55,28 @@ def get_tag_list(url, parser, tag1, tag2, character_decoding):
     return links
 
 
+def send_email_with_urls(urls, sent_from, to, subject, server, username, password):
+    body = "\n\n".join(urls)
+    email_text = """\
+    From: %s
+    To: %s
+    Subject: %s
+
+    %s
+    """ % (sent_from, ", ".join(to), subject, body)
+
+    try:
+        server = smtplib.SMTP_SSL(server, 465)
+        server.ehlo()
+        server.login(username, password)
+        server.sendmail(sent_from, to, email_text)
+        server.close()
+
+        print("Email sent!")
+    except:
+        print("Something went wrong...")
+
+
 def get_desirable_urls():
     try:
         print("BEGIN PROCESS.")
@@ -64,11 +90,14 @@ def get_desirable_urls():
         print("GET TAG LIST")
         tag_links = get_tag_list(p['url_main'], p['parser'],
                                    p['tag1'], p['tag2'], p['character_decoding2'])
-        print(tag_links)
         #get tscainv href link
         print("GET WEBSITE URLS")
-        wildcard_url = get_tag_link(p['url_main'], tag_links, p['wildcard'])
-        print(wildcard_url)
+        wildcard_url = get_tag_link(tag_links, p['wildcard'])
+        if wildcard_url:
+            urls = set(wildcard_url)
+            print(urls)
+            p = parameters['email']
+            send_email_with_urls(urls, [p['email']], [p['email']], p['subject'], p['server'], p['email'], p['password'])
 
     except Exception as e:
         print(str(e))
@@ -76,5 +105,5 @@ def get_desirable_urls():
     finally:
         print("PROCESS COMPLETE.")
 
-
 get_desirable_urls()
+
